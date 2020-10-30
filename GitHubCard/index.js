f935adc26cf563fe8a509d4ad34aee5ba6721018
Kylevb405 <kylevanburen405@gmail.com>
2020-10-29 21:43:58 -0400

    finished project

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,60 +1,186 @@
+import axios from 'axios';
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
 
-/*
-  STEP 2: Inspect and study the data coming back, this is YOUR
-    github info! You will need to understand the structure of this
-    data in order to use it to build your component function
 
-    Skip to STEP 3.
+const cardDiv = document.querySelector('.cards')
+
+
+
+axios.get('https://api.github.com/users/Kylevb405')
+.then((res) => {
+
+  const userData = res.data
+
+  console.log(userData);
+
+  const kyleCard = githubCardComponent(userData)
+
+  // console.log(kyleCard)
+
+  cardDiv.appendChild(kyleCard)  
+  
+  axios.get(`${userData.followers_url}`)
+    .then((res) => {
+  
+      const followerObjects = res.data
+  
+      // console.log(followerObjects);
+  
+  
+      followerObjects.forEach(object => {
+        
+        const followerURL = object.url
+        
+              // console.log(object.url)
+        
+              // console.log(object.login)
+  
+        axios.get(`${followerURL}`)
+          .then((res) => {
+  
+            const followerData = res.data
+  
+            const followerCard = githubCardComponent(followerData)
+  
+            cardDiv.appendChild(followerCard)
+  
+          })
+  
+      });
+  
+      // followerData.forEach( () => {
+  
+      //   // console.log(`this is a test ${object}`);
+  
+      //   const followerCard = githubCardComponent(followerData['url'])
+  
+      //   console.log(followerCard);
+  
+      //   cardDiv.appendChild(followerCard)
+  
+      // })
+          
+    })
+})
+
+
+
+
+
+
+/*
+STEP 2: Inspect and study the data coming back, this is YOUR
+github info! You will need to understand the structure of this
+data in order to use it to build your component function
+
+Skip to STEP 3.
 */
 
 /*
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
-*/
-
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
-
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
-
+    */
+   
+   /*
+   STEP 5: Now that you have your own card getting added to the DOM, either
+   follow this link in your browser https://api.github.com/users/<Your github name>/followers,
+   manually find some other users' github handles, or use the list found at the
+   bottom of the page. Get at least 5 different Github usernames and add them as
+   Individual strings to the friendsArray below.
+   
+   Using that array, iterate over it, requesting data for each user, creating a new card for each
+   user, and adding that card to the DOM.
+   */
+  /*
+    List of LS Instructors Github username's:
+      tetondan
+      dustinmyers
+      justsml
+      luishrd
+      bigknell
+  */
+  
 const followersArray = [];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
 
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
+    <div class="card"> DONE
+      <img src={image url of user} /> DONE
+      <div class="card-info"> DONE
+        <h3 class="name">{users name}</h3> DONE
+        <p class="username">{users user name}</p> DONE
+        <p>Location: {users location}</p> DONE
         <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
+          <a href={address to users github page}>{address to users github page}</a> DONE
         </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
+        <p>Followers: {users followers count}</p>  
+        <p>Following: {users following count}</p> 
         <p>Bio: {users bio}</p>
       </div>
     </div>
 */
 
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
+function githubCardComponent(objData) {
+
+  const gitCard = document.createElement('div')
+    gitCard.classList.add('card')
+
+    const userIMG = document.createElement('img')
+      userIMG.setAttribute('src', objData.avatar_url)
+    
+    const gitCardInfo = document.createElement('div')
+      gitCardInfo.classList.add('card-info')
+    
+    const nameHeader = document.createElement('h3')
+      nameHeader.classList.add('name')
+      nameHeader.textContent = `${objData.name}`
+    
+    const  usernamePTag = document.createElement('p')
+      usernamePTag.classList.add('username')
+      usernamePTag.textContent = `${objData.login}`
+    
+    const locationPTag = document.createElement('p')
+      locationPTag.textContent = `Location: ${objData.location}`
+
+    
+    const profilePtag = document.createElement('p')
+      profilePtag.textContent = `Profile: `
+    
+    const profileAtag = document.createElement('a')
+      profileAtag.setAttribute('href', objData.html_url)
+      profileAtag.textContent = `${objData.html_url}`
+    
+    const followersPtag = document.createElement('p')
+      followersPtag.textContent = `Followers: ${objData.followers}`
+    
+    const followingPTag = document.createElement('p')
+      followingPTag.textContent = `Following: ${objData.following}`
+    
+    const bioPTag = document.createElement('p')
+      bioPTag.textContent = `Bio: ${objData.bio}`
+    
+  gitCard.appendChild(userIMG)
+  gitCard.appendChild(gitCardInfo)
+    gitCardInfo.appendChild(nameHeader)
+    gitCardInfo.appendChild(usernamePTag)
+    gitCardInfo.appendChild(locationPTag)
+    gitCardInfo.appendChild(profilePtag)
+      profilePtag.appendChild(profileAtag)
+    gitCardInfo.appendChild(followersPtag)
+    gitCardInfo.appendChild(followingPTag)
+    gitCardInfo.appendChild(bioPTag)
+    
+  return gitCard
+}
+
+
+
+
+
